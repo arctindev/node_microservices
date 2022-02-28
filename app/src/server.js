9fb6a0ef0bab2209268e7ead1e2d3c154e72867c
Lukasz Kielbasa <arctindev@gmail.com>
2022-02-28 05:11:28 +0100

    Code refactor

--- app/src/server.js
+++ app/src/server.js
@@ -6,16 +6,16 @@ server.get('/db', (req,res) => {
   db.connect()
     .then(client => {
       client.query('SELECT NOW()')
-      .then(data => {
-        console.log('Successfuly connected to db');
-        client.release();
-        res.send({ message: "Success", data : data });
-      })
-      .catch(err => {
-        console.error(err)
-        client.release();
-        res.send({ message: "Failed", data : err });
-      })
+            .then(data => {
+              console.log('Successfuly connected to db');
+              client.release();
+              res.status(200).send({ message: "Success", data : data });
+            })
+            .catch(err => {
+              console.error('Failed to connect to db \n', err)
+              client.release();
+              res.status(400).send({ message: "Failed", data : err });
+            })
     })
 });
 
